f2514f816d62e2f65908fe06be6257533a405ff5
JasonB75 <37903292+JasonB75@users.noreply.github.com>
2019-05-02 11:12:24 -0400

    yes

--- app/src/main/java/com/superking75/testmultiplayer/MainActivity.java
+++ app/src/main/java/com/superking75/testmultiplayer/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 
 import com.google.android.gms.nearby.Nearby;
 import com.google.android.gms.nearby.connection.AdvertisingOptions;
+import com.google.android.gms.nearby.connection.ConnectionsStatusCodes;
 import com.google.android.gms.nearby.connection.Strategy;
 
 import java.util.concurrent.Executor;
@@ -98,4 +99,78 @@ public class MainActivity extends AppCompatActivity {
                             // We're unable to start discovering.
                         });
     }
+    private final EndpointDiscoveryCallback endpointDiscoveryCallback =
+            new EndpointDiscoveryCallback() {
+                @Override
+                public void onEndpointFound(String endpointId, DiscoveredEndpointInfo info) {
+                    // An endpoint was found. We request a connection to it.
+                    Nearby.getConnectionsClient(mContext)
+                            .requestConnection("boi", endpointId, connectionLifecycleCallback)
+                            .addOnSuccessListener(
+                                    (Void unused) -> {
+                                        // We successfully requested a connection. Now both sides
+                                        // must accept before the connection is established.
+                                    })
+                            .addOnFailureListener(
+                                    (Exception e) -> {
+                                        // Nearby Connections failed to request the connection.
+                                    });
+                }
+
+                @Override
+                public void onEndpointLost(String endpointId) {
+                    // A previously discovered endpoint has gone away.;
+                }
+            };
+
+    private final ConnectionLifecycleCallback connectionLifecycleCallback =
+            new ConnectionLifecycleCallback() {
+                @Override
+                public void onConnectionInitiated(String endpointId, ConnectionInfo connectionInfo) {
+                    // Automatically accept the connection on both sides.
+                    Nearby.getConnectionsClient(mContext).acceptConnection(endpointId, payloadCallback);
+                }
+
+                @Override
+                public void onConnectionResult(String endpointId, ConnectionResolution result) {
+                    switch (result.getStatus().getStatusCode()) {
+                        case ConnectionsStatusCodes.STATUS_OK:
+                            // We're connected! Can now start sending and receiving data.
+                            break;
+                        case ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED:
+                            // The connection was rejected by one or both sides.
+                            break;
+                        case ConnectionsStatusCodes.STATUS_ERROR:
+                            // The connection broke before it was able to be accepted.
+                            break;
+                        default:
+                            // Unknown status code
+                    }
+                }
+
+                @Override
+                public void onDisconnected(String endpointId) {
+                    // We've been disconnected from this endpoint. No more data can be
+                    // sent or received.
+                }
+            };
+    
+
+    static class ReceiveBytesPayloadListener extends PayloadCallback {
+
+        @Override
+        public void onPayloadReceived(String endpointId, Payload payload) {
+            // This always gets the full data of the payload. Will be null if it's not a BYTES
+            // payload. You can check the payload type with payload.getType().
+            byte[] receivedBytes = payload.asBytes();
+        }
+
+        @Override
+        public void onPayloadTransferUpdate(String endpointId, PayloadTransferUpdate update) {
+            // Bytes payloads are sent as a single chunk, so you'll receive a SUCCESS update immediately
+            // after the call to onPayloadReceived().
+        }
+    }
 }
+
+
